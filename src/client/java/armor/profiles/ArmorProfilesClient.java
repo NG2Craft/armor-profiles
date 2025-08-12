78940b1adf13c3c8df9d928be3fe1ebd3ea9da8a
quentintyr <quentin.tyr@gmail.com>
2025-08-12 18:14:57 +0200

    ➕ ADD | armor_profiles mod

---
+++ src/client/java/armor/profiles/ArmorProfilesClient.java
@@ -0,0 +1,10 @@
+package armor.profiles;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArmorProfilesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
